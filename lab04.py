6d51792cd376bfe40b08306de5847121e632cc55
kheff16 <kheffernan@umail.ucsb.edu>
2019-10-23 10:12:04 -0700

    Initial Commit

---
+++ lab04.py
@@ -0,0 +1,38 @@
+def createAlphabet(): # 
+    alphabet = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ ,.-~#"
+    return alphabet
+
+def encryptCaesarCipher(plainText, key): 
+    # TODO write function
+
+    """
+
+    Now write a function to perform encryption using the Caesar cipher. 
+    The first few lines of this function need to create your alphabet 
+    using the helpher function you created.
+
+    Then you will have to use a for loop to go through the characters 
+    of your plaintext and for each character you will have to choose 
+    the character from the alphabet that is key positions forward in 
+    the alphabet. Be careful for the cases where you will have to 
+    cycle around the ends of the alphabet.
+
+    """
+    cipherText = ''
+    
+    return cipherText
+
+def decryptCaesarCipher(cipherText, key):
+    # TODO write function
+
+    """
+
+    Finally, write a function that decrypts the Caesar Cipher. 
+    Think: what process would you follow if you were doing the 
+    decryption by hand?
+
+    """
+
+    plainText = ''
+    
+    return plainText
